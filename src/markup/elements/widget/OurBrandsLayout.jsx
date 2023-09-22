e06443dc62d848cf08cdaf060a74e2794c7c6def
jbeanIjas <32436988+jbeanIjas@users.noreply.github.com>
2023-09-22 11:25:07 +0530

    ssss

--- src/markup/elements/widget/OurBrandsLayout.jsx
+++ src/markup/elements/widget/OurBrandsLayout.jsx
@@ -13,7 +13,7 @@ const OurBrandsLayout = (props) => {
       <div className="OurBrandsRow section-sp2">
         <div className="container">
           <div class="heading-bx text-center">
-            <h6 class="title-ext text-primary">Our Prtners</h6>
+            <h6 class="title-ext text-primary">Our Partners</h6>
             <h2 class="title mb-0">BRANDS WE SERVE</h2>
             <p>
             Displaying the brand logos below doesn’t intend to imply or convey the impression that we are authorized to service partners, representatives, dealers, or agents of any of the brands mentioned below. Our intention is merely to categorize and exhibit the types of cars we serve. 
