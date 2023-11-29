77b27c1f607f75cdde7914a09c043ff96fd3fec6
jbeanIjas <32436988+jbeanIjas@users.noreply.github.com>
2023-11-29 16:54:48 +0530

    dddd

--- src/markup/elements/testimonial.jsx
+++ src/markup/elements/testimonial.jsx
@@ -69,7 +69,7 @@ class TestimonialSection extends Component{
 							<h2 className="title mb-0">What our customers say</h2>
 							<p>Check out the reviews from our huge number of satisfied customers to know more about our  quality of servicing and transparency</p>
 						</div>
-						<ElfsightWidget widgetId={"27667d34-8b84-4b11-8a87-f4198485ba57"} lazy />
+						<ElfsightWidget widgetId={"a4fa245b-d2eb-4a8c-ac98-db1a9734c6aa"} lazy />
 					</div>
 					<div className='Mask'>&nbsp;</div>
 				</section>
