21ff0031f74ec3c36f81fd8bd2989d5b82385ed6
de-salman <salmanfarisi.bca@gmail.com>
2023-12-11 07:52:54 +0530

    11-12

--- src/markup/elements/job-app.jsx
+++ src/markup/elements/job-app.jsx
@@ -1,42 +1,51 @@
-// JobApplicationForm.js
+
 
 import React, { useEffect } from 'react';
 
 const JobApplicationForm = () => {
   useEffect(() => {
-    const handleIFrameMessage = (e) => {
-      // Your existing handleIFrameMessage logic here
-      // ...
+    try {
+      const f = document.createElement("iframe");
+      f.src = 'https://forms.zohopublic.in/salman2/form/swiss/formperma/B5gwauxmZBHmZZAjZL2mrSFXmmHI-kDT4ACcTmab4KY?zf_rszfm=1';
+      f.style.border = "none";
+      f.style.height = "739px";
+      f.style.width = "90%";
+      f.style.transition = "all 0.5s ease";
+      f.setAttribute("aria-label", 'swiss');
 
-      // Example: Log the message data
-      console.log('Received message:', e.data);
-    };
+      const d = document.getElementById("zf_div_B5gwauxmZBHmZZAjZL2mrSFXmmHI-kDT4ACcTmab4KY");
+      d.appendChild(f);
 
-    if (window.addEventListener) {
-      window.addEventListener('message', handleIFrameMessage, false);
-    } else if (window.attachEvent) {
-      window.attachEvent('onmessage', handleIFrameMessage);
+      const handleMessage = (event) => {
+        const evntData = event.data;
+        if (evntData && evntData.constructor === String) {
+          const zf_ifrm_data = evntData.split("|");
+          if (zf_ifrm_data.length === 2) {
+            const zf_perma = zf_ifrm_data[0];
+            const zf_ifrm_ht_nw = (parseInt(zf_ifrm_data[1], 10) + 15) + "px";
+            const iframe = document.getElementById("zf_div_B5gwauxmZBHmZZAjZL2mrSFXmmHI-kDT4ACcTmab4KY").getElementsByTagName("iframe")[0];
+            if ((iframe.src).indexOf('formperma') > 0 && (iframe.src).indexOf(zf_perma) > 0) {
+              const prevIframeHeight = iframe.style.height;
+              if (prevIframeHeight !== zf_ifrm_ht_nw) {
+                iframe.style.height = zf_ifrm_ht_nw;
+              }
+            }
+          }
+        }
+      };
+
+      window.addEventListener('message', handleMessage, false);
+
+      return () => {
+        window.removeEventListener('message', handleMessage, false);
+      };
+    } catch (e) {
+      console.error(e);
     }
-
-    // Cleanup event listener on component unmount
-    return () => {
-      if (window.removeEventListener) {
-        window.removeEventListener('message', handleIFrameMessage);
-      } else if (window.detachEvent) {
-        window.detachEvent('onmessage', handleIFrameMessage);
-      }
-    };
-  }, []); // Empty dependency array to ensure the effect runs only once
+  }, []);
 
   return (
-    <iframe
-      id="JotFormIFrame-233381171205447"
-      title="Online Job Application Form"
-      // Your existing iframe attributes here
-      src="https://form.jotform.com/233381171205447"
-      style={{ minWidth: '100%', maxWidth: '100%', height: '1000px', border: 'none' }}
-      scrolling="no"
-    />
+    <div id="zf_div_B5gwauxmZBHmZZAjZL2mrSFXmmHI-kDT4ACcTmab4KY"></div>
   );
 };
 
--- src/markup/pages/job-career.jsx
+++ src/markup/pages/job-career.jsx
@@ -1,4 +1,4 @@
-import React, {Component} from 'react';
+import React, {Component, useState} from 'react';
 import { Link } from 'react-router-dom';
 
 // Layout
@@ -12,9 +12,9 @@ import WidgetContact from "../elements/widget/widget-contact";
 import bnrImg from "../../images/banner/bnr2.jpg";
 import JobApplicationForm from '../elements/job-app';
 
-class JobCareer extends Component{
+function JobCareer() {
+
 	
-	render(){
 		return (
 			<>
 				
@@ -46,74 +46,6 @@ class JobCareer extends Component{
 										<p className="head-px2">Our purpose follow leading design, engineering and industrial manufacturing company specialising in mining, oil, gas and engineering services.</p>
 									</div>
 									<JobApplicationForm />
-									<div className="row">
-										<div className="col-md-12 mb-30">
-											<div className="job-career-box">
-												<h4>ENGINE REPAIR</h4>
-												<p><strong>Position:</strong> Mechanical Engineering</p>
-												<p><strong>Experience:</strong> 4 5 years</p>
-												<p><strong>Location:</strong> 2005 Stokes Isle Apt. 896, Venaville 10010, USA</p>
-												<p><strong>Minimum Qualification Requirement :</strong> MBA</p>
-												<p><strong>Job Description:</strong> There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
-												<p><strong>Key Responsibilities:</strong> If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
-												<ul className="list-check">
-													<li>Extensive experience with WordPress architecture, custom content types and plugins</li>
-													<li>Strong understanding of PHP</li>
-													<li>Solid foundation in front-end technologies such as HTML5, CSS3, Javascript and Ajax is plus.</li>
-													<li>Experience with version control (Git preferred)</li>
-													<li>Knowledge of best practices (standard naming conventions, code styles, security, etc.)</li>
-													<li>Experience with Adobe Photoshop</li>
-													<li>Intermediate understanding of coding SEO-friendly sites</li>
-												</ul>
-												<p><strong>Soft Skills:</strong> It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
-												<Link to="/booking" className="btn btn-primary mt-10">Apply Now</Link>
-											</div>
-										</div>
-										<div className="col-md-12 mb-30">
-											<div className="job-career-box">
-												<h4>BREAK REPAIR</h4>
-												<p><strong>Position:</strong> Mechanical Engineering</p>
-												<p><strong>Experience:</strong> 4 5 years</p>
-												<p><strong>Location:</strong> 2005 Stokes Isle Apt. 896, Venaville 10010, USA</p>
-												<p><strong>Minimum Qualification Requirement :</strong> MBA</p>
-												<p><strong>Job Description:</strong> There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
-												<p><strong>Key Responsibilities:</strong> If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
-												<ul className="list-check">
-													<li>Extensive experience with WordPress architecture, custom content types and plugins</li>
-													<li>Strong understanding of PHP</li>
-													<li>Solid foundation in front-end technologies such as HTML5, CSS3, Javascript and Ajax is plus.</li>
-													<li>Experience with version control (Git preferred)</li>
-													<li>Knowledge of best practices (standard naming conventions, code styles, security, etc.)</li>
-													<li>Experience with Adobe Photoshop</li>
-													<li>Intermediate understanding of coding SEO-friendly sites</li>
-												</ul>
-												<p><strong>Soft Skills:</strong> It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
-												<Link t="/booking" className="btn btn-primary mt-10">Apply Now</Link>
-											</div>
-										</div>
-										<div className="col-md-12 mb-30">
-											<div className="job-career-box">
-												<h4>CAR PAINTING</h4>
-												<p><strong>Position:</strong> Mechanical Engineering</p>
-												<p><strong>Experience:</strong> 4 5 years</p>
-												<p><strong>Location:</strong> 2005 Stokes Isle Apt. 896, Venaville 10010, USA</p>
-												<p><strong>Minimum Qualification Requirement :</strong> MBA</p>
-												<p><strong>Job Description:</strong> There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration in some form, by injected humour, or randomised words which don't look even slightly believable.</p>
-												<p><strong>Key Responsibilities:</strong> If you are going to use a passage of Lorem Ipsum, you need to be sure there isn't anything embarrassing hidden in the middle of text. All the Lorem Ipsum generators on the Internet tend to repeat predefined chunks as necessary, making this the first true generator on the Internet.</p>
-												<ul className="list-check">
-													<li>Extensive experience with WordPress architecture, custom content types and plugins</li>
-													<li>Strong understanding of PHP</li>
-													<li>Solid foundation in front-end technologies such as HTML5, CSS3, Javascript and Ajax is plus.</li>
-													<li>Experience with version control (Git preferred)</li>
-													<li>Knowledge of best practices (standard naming conventions, code styles, security, etc.)</li>
-													<li>Experience with Adobe Photoshop</li>
-													<li>Intermediate understanding of coding SEO-friendly sites</li>
-												</ul>
-												<p><strong>Soft Skills:</strong> It uses a dictionary of over 200 Latin words, combined with a handful of model sentence structures, to generate Lorem Ipsum which looks reasonable. The generated Lorem Ipsum is therefore always free from repetition, injected humour, or non-characteristic words etc.</p>
-												<Link to="/booking" className="btn btn-primary mt-10">Apply Now</Link>
-											</div>
-										</div>
-									</div>
 								</div>
 								<div className="col-lg-4 col-md-5 mb-20">
 									<aside className="sticky-top">
@@ -134,7 +66,6 @@ class JobCareer extends Component{
 				
 			</>
 		);
-	}
 }
 
 export default JobCareer;
