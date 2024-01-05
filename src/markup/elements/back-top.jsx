9db9a50233e949c8a7fb09c66144be7c9899646d
de-salman <salmanfarisi.bca@gmail.com>
2024-01-05 17:20:54 +0400

    05-01 2

--- src/markup/elements/back-top.jsx
+++ src/markup/elements/back-top.jsx
@@ -1,43 +1,68 @@
 import React, { Component } from 'react';
-import {animateScroll} from 'react-scroll';
+import { animateScroll } from 'react-scroll';
 
-class BackToTop extends Component{
-	
-	scrollToTop = () => {
-        animateScroll.scrollToTop();
-	}
-	componentDidMount = () => {
-        window.addEventListener("scroll",()=>{
-            if(window.pageYOffset > this.state.offset){
-                this.setState({
-                    ...this.state,
-                    show:true
-                })
-            }
-			else{
-                this.setState({
-                    ...this.state,
-                    show:false
-                })
-            }
-        })
-	}
-	state = { 
-        offset : this.props.offset || 1000,
-        show   : false
+class BackToTop extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            offset: this.props.offset || 1000,
+            show: false
+        };
     }
-	
-	render(){
-		return(
-			<>
-				{this.state.show && (
+
+    scrollToTop = () => {
+        animateScroll.scrollToTop();
+    };
+
+    handleAnchorLinkClick = (event) => {
+        const { target } = event;
+
+        // Check if the clicked target is an anchor link
+        if (target.tagName.toLowerCase() === 'a' && target.hash) {
+            const anchor = target.hash.substring(1);
+            const element = document.getElementById(anchor);
+
+            if (element) {
+                event.preventDefault();
+                element.scrollIntoView();
+            }
+        }
+    };
+
+    componentDidMount() {
+        window.addEventListener('scroll', () => {
+            if (window.pageYOffset > this.state.offset) {
+                this.setState({
+                    ...this.state,
+                    show: true
+                });
+            } else {
+                this.setState({
+                    ...this.state,
+                    show: false
+                });
+            }
+        });
+
+        // Handle anchor link clicks within the component
+        document.addEventListener('click', this.handleAnchorLinkClick);
+    }
+
+    componentWillUnmount() {
+        document.removeEventListener('click', this.handleAnchorLinkClick);
+    }
+
+    render() {
+        return (
+            <>
+                {this.state.show && (
                     <button className="back-to-top" type="button" onClick={this.scrollToTop}>
-						<i className="fa fa-chevron-up"></i>
+                        <i className="fa fa-chevron-up"></i>
                     </button>
                 )}
-			</>
-		);
-	}
+            </>
+        );
+    }
 }
 
 export default BackToTop;
